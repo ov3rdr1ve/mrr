eda077578062dc4262cfe9562798bb7d5bbbdc65
Kirill K <33423748+ov3rdr1ve@users.noreply.github.com>
2021-07-12 14:07:54 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+$parms = array(
+	"rig"=>196918,
+	"length"=>24,
+	"rate"=>array(
+		"type"=>"mh",
+		"price"=>0.09330000,
+	),
+	"currency"=>"LTC",
+	"profile"=>142727,
+	//"profile"=>149532,
+	//"profile"=>152427,
+	
+);
