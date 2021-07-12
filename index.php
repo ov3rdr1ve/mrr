90564139ce69ef537a0812946969540950137d14
Kirill K <33423748+ov3rdr1ve@users.noreply.github.com>
2021-07-12 14:31:04 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-"rig"=>196918,
+$parms = array(
+	"rig"=>196918,
 	"length"=>24,
 	"rate"=>array(
 		"type"=>"mh",
@@ -8,3 +9,5 @@
 	"profile"=>142727,
 	//"profile"=>149532,
 	//"profile"=>152427,
+	
+);
