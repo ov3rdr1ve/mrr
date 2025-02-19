d75cb6337b712e1f36998f715c846cd70ac2e3f4
Kirill K <33423748+ov3rdr1ve@users.noreply.github.com>
2025-02-19 16:46:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,1 @@
-<?php
 
-$parms = array(
-	"rig"=>196918,
-	"length"=>24,
-	"rate"=>array(
-		"type"=>"mh",
-		"price"=>0.09330000,
-	),
-	"currency"=>"LTC",
-	"profile"=>142727,
-	//"profile"=>149532,
-	//"profile"=>152427,
-	
-);
-
-?>
